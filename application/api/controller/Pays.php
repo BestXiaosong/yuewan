78935bc7e993dc3e072d93e59edf73e5db7b26f7
BestXiaosong <306027376@qq.com>
2018-12-10 16:54:55 +0800

    支付调试

--- application/api/controller/Pays.php
+++ application/api/controller/Pays.php
@@ -185,13 +185,13 @@ class Pays extends User
      */
     public function notify()
     {
-        $alipay = Pay::alipay($this->alipay);
+//        $alipay = Pay::alipay($this->alipay);
         try{
-            $data = $alipay->verify(); // 是，验签就这么简单！
-            cache('aliTest',$data);
-//            $data = cache('aliTest');
-//
-//            dd($data);
+//            $data = $alipay->verify(); // 是，验签就这么简单！
+//            cache('aliTest',$data);
+            $data = cache('aliTest');
+
+            dd($data);
 
             // 请自行对 trade_status 进行判断及其它逻辑进行判断，在支付宝的业务通知中，只有交易通知状态为 TRADE_SUCCESS 或 TRADE_FINISHED 时，支付宝才会认定为买家付款成功。
             // 1、商户需要验证该通知数据中的out_trade_no是否为商户系统中创建的订单号；
