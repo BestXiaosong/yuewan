934baac5d747734044dea615cce036980e93d2e8
bseXiaosong <306027376@qq.com>
2018-12-10 19:41:07 +0800

    融云IM组件引入

--- application/api/controller/Pays.php
+++ application/api/controller/Pays.php
@@ -194,9 +194,9 @@ class Pays extends User
     {
         $alipay = Pay::alipay($this->alipay);
         try{
-            $data = $alipay->verify(); // 是，验签就这么简单！
-            cache('aliTest',$data);
-//            $data = cache('aliTest');
+//            $data = $alipay->verify(); // 是，验签就这么简单！
+//            cache('aliTest',$data);
+            $data = cache('aliTest');
 
 
             // 请自行对 trade_status 进行判断及其它逻辑进行判断，在支付宝的业务通知中，只有交易通知状态为 TRADE_SUCCESS 或 TRADE_FINISHED 时，支付宝才会认定为买家付款成功。
@@ -274,7 +274,7 @@ class Pays extends User
      * 礼物赠送回调成功处理
      */
     protected function gift($data){
-
+        dd($data);
         $userIds = explode(',',$data['to_user']);
 
         $item['room_id'] = $data['room_id']??0;
